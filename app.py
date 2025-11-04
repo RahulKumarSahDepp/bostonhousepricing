a0d1c7c3349ff791824ef6b882f9371ab48710f2
Rahul kumar sah <scanningforuh@gmail.com>
2025-11-04 18:49:14 +0545

    final web app

--- app.py
+++ app.py
@@ -22,6 +22,15 @@ def predict_api():
     print(output)
     return jsonify(output[0])
 
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = [float(x) for x in request.form.values()]
+    final_input = scalar.transform(np.array(data).reshape(1, -1))
+    print(final_input)
+    output = regmodel.predict(final_input)[0]
+    return render_template('index.html', prediction_text = "The predicted house price is {}".format(output))
+
+
 if "__main__" == __name__:
     app.run(debug=True)
 
